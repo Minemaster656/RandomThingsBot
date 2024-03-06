3f92909d53511504d0b635573d959f653ed26c9a
Minemaster656 <84704268+Minemaster656@users.noreply.github.com>
2024-03-06 19:13:00 +0400

    Ребаланс суммари чата

--- cogs/AI_things.py
+++ cogs/AI_things.py
@@ -206,10 +206,10 @@ class AI_things(commands.Cog):
                 #
                 # await ctx.send(content)
 
-    @commands.cooldown(1, 60, commands.BucketType.channel)
+    @commands.cooldown(1, 600, commands.BucketType.channel)
     @commands.command(aliases=["пересказать-чат", "перескажи-чат"])
     async def summarize(self, ctx: commands.Context):
-        history_size = 40
+        history_size = 64
 
         messages = await ctx.channel.history(limit=history_size).flatten()
         # messages.reverse()
