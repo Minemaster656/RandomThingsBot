b2920ca81980320f31eb04bb0df842dea9a86bd6
minemaster <glebbasmanov123@gmail.com>
2024-07-25 13:13:18 +0400

    возможно работает

--- cogs/TTS.py
+++ cogs/TTS.py
@@ -17,45 +17,15 @@ class TTS(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
 
-    @commands.Cog.listener("on_voice_state_update")
-    async def on_voice_state_update(self, member, before, after):
-        if not member.bot:
-            if before.channel is not None and after.channel is None:
-                voice_channel = before.channel
-                if len(voice_channel.members) == 1:  # Проверяем, что остался только один участник (бот)
-                    await voice_channel.guild.voice_client.disconnect()
-                    self.tts_channels.pop(voice_channel.id)
-    @commands.slash_command(name="включить-говорилку",description="Включает озвучку чата войса говорилкой от гугла. Если она работает, конечно же...")
-    async def initTTS(self, ctx):
-        member = ctx.guild.get_member(ctx.author.id)
-        if not member:
-            await ctx.respond("Вы должны быть на сервере!", ephemeral=True)
-            return
-        if not member.voice:
-            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
-            return
-        if not member.voice.channel:
-            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
-            return
-        channel = member.voice.channel
-        if isinstance(channel, discord.StageChannel):
-            await ctx.respond("Вы можете использовать говорилку ТОЛЬКО в голосовых каналах. На сценах она отключена!", ephemeral=True)
-            return
-        if channel.guild.voice_client is not None:
-            await channel.guild.voice_client.disconnect()
-        vc = await channel.connect(cls=discord.VoiceClient)
-        await channel.send("Говорилка включена!")
-        self.tts_channels[channel.id] = vc
-        # print("ГОВОРИЛКА", vc)
 
-    @commands.Cog.listener("on_message")
-    async def TTS_on_message(self, message: discord.Message):
-        if not isinstance(message.channel, discord.VoiceChannel):
-            return
-        if not message.guild.me.voice or not message.channel.id in self.tts_channels:
-            return
-        if len(message.content) == 0:
-            return
+
+        self.tts_background.start()
+
+    def cog_unload(self):
+        self.tts_background.cancel()
+
+    async def TTSify(self, message):
+        print(message.content)
         # vc = message.guild.me.voice.channel
         TTS = libs.CachedTTS.CachedTTS(f"files/TTS", f"files/db/TTS.db")
         # print(TTS.path_to_audio)
@@ -92,19 +62,108 @@ class TTS(commands.Cog):
         #             sequence.remove("speaks_tts")
         for key in ttss.keys():
             if ttss[key] is None:
-                self.tts_channels[message.channel.id].play(discord.FFmpegPCMAudio(f"assets/tts_error.mp3"), after=lambda e: ...)
+                self.tts_channels[message.channel.id]["vc"].play(discord.FFmpegPCMAudio(f"assets/tts_error.mp3"),
+                                                                 after=lambda e: ...)
+
         async def play_audio(vc, audio_files):
             for file in audio_files:
                 # print(file)
                 # print(file.__class__)
                 # print(isinstance(file, libs.CachedTTS.TTS_phrase))
                 if file == audio_files[-1]:
-                    vc.play(discord.FFmpegPCMAudio(f"{file.path}/{file.filename}.mp3", options=f"-filter:a 'atempo={self.SPEED_TTS}'"), after=lambda e: ...)
+                    vc.play(discord.FFmpegPCMAudio(f"{file.path}/{file.filename}.mp3",
+                                                   options=f"-filter:a 'atempo={self.SPEED_TTS}'"), after=lambda e: ...)
                 else:
-                    vc.play(discord.FFmpegPCMAudio(f"{file.path}/{file.filename}.mp3", options=f"-filter:a 'atempo={self.SPEED}'"), after=lambda e: ...)
+                    vc.play(discord.FFmpegPCMAudio(f"{file.path}/{file.filename}.mp3",
+                                                   options=f"-filter:a 'atempo={self.SPEED}'"), after=lambda e: ...)
                 while vc.is_playing():
                     await asyncio.sleep(0.01)
-        await play_audio(self.tts_channels[message.channel.id], sequence)
+
+        await play_audio(self.tts_channels[message.channel.id]["vc"], sequence)
+
+    @commands.Cog.listener("on_voice_state_update")
+    async def on_voice_state_update(self, member, before, after):
+        if not member.bot:
+            if before.channel is not None and after.channel is None:
+                voice_channel = before.channel
+                if len(voice_channel.members) == 1:  # Проверяем, что остался только один участник (бот)
+                    await voice_channel.guild.voice_client.disconnect()
+                    self.tts_channels.pop(voice_channel.id)
+        if member.bot:
+            if before.channel is not None and after.channel is None:
+                voice_channel = before.channel
+                self.tts_channels.pop(voice_channel.id)
+
+    @tasks.loop(seconds=0.1)
+    async def tts_background(self):
+        print("bg loop")
+        print(self.tts_channels)
+        for TTS_client in self.tts_channels:
+            if not self.tts_channels[TTS_client]["vc"].is_playing():
+                if len(TTS_client.queue) == 0:
+                    continue
+                else:
+                    await self.TTSify(TTS_client.queue[0])
+                    TTS_client.queue.pop(0)
+
+
+    @commands.slash_command(name="включить-говорилку",description="Включает озвучку чата войса говорилкой от гугла. Если она работает, конечно же...")
+    async def initTTS(self, ctx):
+        member = ctx.guild.get_member(ctx.author.id)
+        if not member:
+            await ctx.respond("Вы должны быть на сервере!", ephemeral=True)
+            return
+        if not member.voice:
+            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
+            return
+        if not member.voice.channel:
+            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
+            return
+        channel = member.voice.channel
+        if isinstance(channel, discord.StageChannel):
+            await ctx.respond("Вы можете использовать говорилку ТОЛЬКО в голосовых каналах. На сценах она отключена!", ephemeral=True)
+            return
+        if channel.guild.voice_client is not None:
+            await channel.guild.voice_client.disconnect()
+        vc = await channel.connect(cls=discord.VoiceClient)
+        await channel.send("Говорилка включена!")
+        self.tts_channels[channel.id] = {"vc":vc, "queue":[]}
+        # print("ГОВОРИЛКА", vc)
+    @commands.slash_command(name="отключить-говорилку",description="выкидывает бота из войса и выключает эту ваши говорилку")
+    async def disconnectSpeaker(self, ctx: discord.ApplicationContext):
+        member = ctx.guild.get_member(ctx.author.id)
+        if not member:
+            await ctx.respond("Вы должны быть на сервере!", ephemeral=True)
+            return
+        if not member.voice:
+            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
+            return
+        if not member.voice.channel:
+            await ctx.respond("Вы должны быть в голосовом чате!", ephemeral=True)
+            return
+        channel = member.voice.channel
+        if member.guild.me.voice.channel.id != channel.id:
+            await ctx.respond("Вы должны быть в голосовом чате с ботом!", ephemeral=True)
+            return
+        await channel.guild.voice_client.disconnect()
+        await ctx.respond("Говорилка выключена!")
+
+    @commands.Cog.listener("on_message")
+    async def TTS_on_message(self, message: discord.Message):
+        if not isinstance(message.channel, discord.VoiceChannel):
+            return
+        if not message.guild.me.voice or not message.channel.id in self.tts_channels.keys():
+            return
+        if len(message.content) == 0:
+            return
+        if message.author.id == self.bot.user.id:
+            print("ids are same")
+            return
+        print(self.tts_channels)
+        print(message.content)
+        self.tts_channels[message.channel.id]["queue"].append(message)
+        print(self.tts_channels)
+
 
 
 
